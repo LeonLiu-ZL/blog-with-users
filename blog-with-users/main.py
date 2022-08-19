357779165340419b0c0471552535530d3c645a5b
LeonLiu-ZL <bioeditor@foxmail.com>
2022-08-19 11:47:37 +0800

    Change dependency: SQLAlchemy==1.3.19; Change Config Vars: os.environ.get("DATABASE_URL"): Add back db.create_all()

--- blog-with-users/main.py
+++ blog-with-users/main.py
@@ -19,7 +19,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -64,7 +64,7 @@ class Comment(db.Model):
     blog = relationship("BlogPost", back_populates='comment')
 
 
-# db.create_all()
+db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
